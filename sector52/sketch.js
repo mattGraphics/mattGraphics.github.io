9f007910be098ff93c302a331a1ec91ee3afc4ed
BA Graphics <36536330+mattGraphics@users.noreply.github.com>
2020-10-20 14:40:24 +0100

    Create sketch.js

---
+++ sector52/sketch.js
@@ -0,0 +1,46 @@
+let perps = ['john','jack','jim'];
+let places = ['place a','place b','place c'];
+let crimes = ['mugging','stabbing','jaywalking'];
+
+let perp = "";
+let place = "";
+let crime = "";
+
+let button;
+
+let cnv;
+
+
+function setup() {
+  cnv = createCanvas(400, 400);
+  cnv.position(width/2, height/2, 'relative');
+  background(0);
+  
+  button = createButton('generate');
+  button.position(cnv.x+50, cnv.y+50);
+  button.mousePressed(generate);
+  button.addClass('bigB');
+  
+  textSize(30);
+  fill(255);
+  //textAlign(CENTER, CENTER);
+  
+}
+
+function draw() {
+  //background(220);
+  
+
+}
+
+function generate()
+{
+  background(0);
+  perp = random(perps);  
+  place = random(places); 
+  crime = random(crimes); 
+  
+  text(perp+" in "+place+" committed "+crime, 50, 120, 300,300);
+  
+
+}
