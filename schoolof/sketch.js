e6a58b8f2d003a19a41a6316dc0558acc7c2fc82
BA Graphics <36536330+mattGraphics@users.noreply.github.com>
2020-11-17 16:06:46 +0000

    Add files via upload

---
+++ schoolof/sketch.js
@@ -0,0 +1,173 @@
+//LEFT TEXT VARIABLES
+var leftText;
+var leftArray = [];
+var counterL = 1;
+var newLPos;
+var scalingL;
+
+//RIGHT TEXT VARIABLES
+var rightText;
+var rightArray = [];
+var counterR = 1;
+var newRPos;
+var scalingR;
+
+//SHARED VARIABLES
+var wordHeight;
+var txtSize = 30;
+var sine = 0.01;
+
+//BG ''&'' CODE
+let graphic
+let font
+let canvas
+const loopDuration = 3 * 60
+
+
+
+function preload() {
+  leftText = loadStrings('words02.txt');
+  rightText = loadStrings('words01.txt');
+  bodyCopy = loadFont('abril.otf');
+}
+
+
+function setup() {
+  createCanvas(800, 800);
+  wordHeight = height/leftText.length;
+  textFont(bodyCopy);
+
+  //LEFT TEXT
+  for(var i=0; i<leftText.length;i++)
+    {
+      leftArray.push({words:leftText[i], yPos:wordHeight*i});
+      if(leftArray[i].words === 'Architectural Design & Technology')
+        {
+          leftArray[i].words = 'Architectural Design'+'\n'+'& Technology';
+        }
+    }
+  
+  //RIGHT TEXT
+  for(var i=0; i<rightText.length;i++)
+    {
+      rightArray.push({words:rightText[i], yPos:wordHeight*i});
+    }
+  
+  
+  //BG '&' CODE
+  // create offscreen graphics buffer
+  graphic = createGraphics(width, height)
+  
+  // type setup offscreen in buffer
+  graphic.textFont(bodyCopy)
+  graphic.textAlign(CENTER, CENTER)
+  graphic.blendMode(SCREEN)
+  graphic.textSize(200)
+  graphic.fill('rgba(255, 0, 0, 0.9)')
+  graphic.text('&', width / 2, height / 2)
+  graphic.textSize(210)
+  graphic.fill('rgba(0, 255, 0, 0.9)')
+  graphic.text('&', width / 2, height / 1.98)
+  graphic.textSize(190)
+  graphic.fill('rgba(0, 0, 255, 0.9)')
+  graphic.text('&', width / 2, height / 2.02)
+  
+  
+}
+
+function draw() {
+  clear();
+  
+ 
+  
+   //BG '&' CODE
+  let currentFrame = frameCount % loopDuration
+  let t = currentFrame / loopDuration
+  let u = map(t, 0, 1, 0, 2 * PI)
+
+  
+  const tiles = 24
+  //const tileSize = width / tiles
+  tileSize = 50
+  
+  // loop over each tile
+  for (let x = 0; x < tiles; x++) {
+    for (let y = 0; y < tiles; y++) {
+      
+      const distortionX = cos(u + x * 0.5) * 10
+      const distortionY = sin(u + y * 0.5) * 10
+      
+      // think of this as applying the grid to the source in the graphics buffer
+      const sx = x * tileSize + distortionX
+      const sy = y * tileSize + distortionY
+      const sw = tileSize + distortionX
+      const sh = tileSize + distortionY
+
+      // and this as applying the grid to the destination on the canvas
+      const dx = x * tileSize
+      const dy = y * tileSize
+      const dw = tileSize
+      const dh = tileSize
+
+      // grided image from buffer into main canvas
+      image(graphic, dx, dy, dw, dh, sx, sy, sw, sh)
+    }
+  }
+  
+  
+  
+  
+  //LEFT TEXT
+         textAlign(RIGHT, TOP);
+          var s = map(sin(sine),-1,1,0,3);
+        
+          for(var i=0; i<leftArray.length; i++)
+            {
+              leftArray[i].yPos += s;
+              scalingL = abs(map(leftArray[i].yPos, 0, height, -1,1));
+              textSize(txtSize-(txtSize*scalingL));
+              fill(255,255-(255*scalingL));
+              
+              if(leftArray[i].yPos >= height)
+                {
+                  leftArray[i].yPos -= height;
+                  leftArray.splice(0, 0, leftArray[i]);
+                   leftArray.pop();
+                }
+              
+              text(leftArray[i].words,-50,leftArray[i].yPos,width/2,height);
+            }
+
+  
+  //RIGHT TEXT
+          textAlign(LEFT, TOP);
+        
+          for(var i=0; i<rightArray.length; i++)
+            {
+              rightArray[i].yPos -= s;
+              scalingR = abs(map(rightArray[i].yPos, 0, height, -1,1));
+              textSize(txtSize-(txtSize*scalingR));
+              fill(255,255-(255*scalingR));
+              
+               if(rightArray[i].yPos < 0)
+                {
+                  rightArray[i].yPos = (rightArray.length*wordHeight)-rightArray[i].yPos;
+                  rightArray.push(rightArray[i]);
+                   rightArray.splice(0,1);
+                }
+              
+              text(rightArray[i].words,width/2+50,rightArray[i].yPos-7,width/2,height);
+            }
+  
+
+  sine+=TWO_PI/44.5;
+  
+  
+  textAlign(CENTER,TOP);
+  fill(238,116,225);
+  textSize(100);
+  text("School of",0,0,width,height);
+ 
+  
+  
+}
